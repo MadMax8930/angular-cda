a6b124f3f3f0970253e1ac34ab89413461a9be50
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-06-27 16:14:21 +0200

    9 - icons component

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconNavComponent } from './components/icon-nav/icon-nav.component';
+import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
+import { IconCloseComponent } from './components/icon-close/icon-close.component';
+import { IconEditComponent } from './components/icon-edit/icon-edit.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    IconNavComponent,
+    IconDeleteComponent,
+    IconCloseComponent,
+    IconEditComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    IconNavComponent,
+    IconDeleteComponent,
+    IconCloseComponent,
+    IconEditComponent
   ]
 })
 export class IconsModule { }
