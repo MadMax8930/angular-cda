9864e0da1e62736963e894f22c27ed21b7c4f0c4
Jérémy Bojko <jeremy.bojko@protonmail.com>
2022-06-27 15:26:51 +0200

    8 - ngclass event binding

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ui.component.scss']
 })
 export class UiComponent implements OnInit {
+  public isNavOpen: boolean;
 
-  constructor() { }
+  constructor() {
+    this.isNavOpen = true;
+  }
 
   ngOnInit(): void {
+  }
+
+  public toggle(): void {
+    this.isNavOpen = !this.isNavOpen;
   }
 
 }
